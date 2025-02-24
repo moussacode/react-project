175db72c1b3caa02cd3c93149c70db6df793ac41
moussacode <samouh269@gmail.com>
2025-02-24 12:15:11 +0000

    Revert "Merge pull request #1 from moussacode/dev"
    This reverts commit 8a828a6172befae8daebfde34dd916f7e81087b1, reversing
    changes made to 4d3e41394b2bc12b6afa1f0c0d8a747a3cbf979e.

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,13 @@
 
 import './App.css'
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import AuthPage from './page/LandingPage';
-import AdminDashboard from './page/AdminDashboard';
-import SellerSpace from './page/SellerSpace';
-import ClientShop from './page/ClientShop';
-import LoginPage from './page/LoginPage';
-import RegisterPage from './page/RegisterPage';
-import LandingPage from './page/LandingPage';
-
-
-
-function App () {
+function App() {
+  
   return (
-    <Router>
-      <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
-        <Route path="/admin" element={<AdminDashboard />} />
-        <Route path="/vendeur" element={<SellerSpace />} />
-        <Route path="/boutique" element={<ClientShop />} />
-      </Routes>
-    </Router>
-  );
+    <>
+      <h1 className="text-center text-3xl font-bold  ">Hello World</h1>
+    </>
+  )
 }
-export default App;
+
+export default App
